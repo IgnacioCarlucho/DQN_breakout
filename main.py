fixed issues with loss and gradient

main.py
@@ -6,8 +6,9 @@ import gym
 import wrappers as wp
 import cv2
 import time
+from collections import deque
 DEVICE = '/gpu:0'
-
+np.set_printoptions(threshold=np.nan)
 # Base learning rate 
 LEARNING_RATE = 4*1e-4
 # Soft target update param
@@ -24,10 +25,11 @@ def trainer(epochs=1000, MINIBATCH_SIZE=32, GAMMA = 0.99,save=1, save_image=1, e
         tf.set_random_seed(RANDOM_SEED)
         # set evironment
         # robot = gym_environment('FrozenLakeNonskid4x4-v3', False, False, False) 
+        # breakout
         #env = gym.make('BreakoutDeterministic-v4')
-        env = wp.wrap_dqn(gym.make('BreakoutDeterministic-v4'))
+        #env = wp.wrap_dqn(gym.make('BreakoutDeterministic-v4'))
         # Pong-v0
-        #env= wp.wrap_dqn(gym.make('PongDeterministic-v4'))
+        env= wp.wrap_dqn(gym.make('PongDeterministic-v4'))
         agent = Network(sess,SIZE_FRAME,N_ACTIONS,LEARNING_RATE,DEVICE)
         #IG=imageGrabber()
         
@@ -57,34 +59,34 @@ def trainer(epochs=1000, MINIBATCH_SIZE=32, GAMMA = 0.99,save=1, save_image=1, e
         algo = 0
         total_frames_counter = 0 
         frames_number = 0
+        frames_to_save = 0
         while total_frames_counter < 4000000:
             
-            if (total_frames_counter%500 == 0) : 
-                print('*************************')
-                print('now we save the model')
+            if frames_to_save > 10000:
                 agent.save()
-                #replay_buffer.save()
-                print('model saved succesfuly')
-                print('*************************')
-                
-            if frames_number > 10000: 
-                 agent.update_target_network()
-                 frames_number = 0
-                 print('update_target_network')
+                frames_to_save = 0
 
-            
+            if frames_number > 1000:#10000: 
+                agent.update_target_network()
+                frames_number = 0
+                print('update_target_network and save network')
+                # agent.save()
+                # replay_buffer.save()
             
             state = env.reset()
             q0 = np.zeros(N_ACTIONS)
             ep_reward = 0.
             done = False
             step = 0
+            total_loss = deque()
+            loss = 0.
             while not done:
                
                 frames_number = frames_number + 1
+                frames_to_save = frames_to_save + 1 
                 total_frames_counter = total_frames_counter + 1
                 if total_frames_counter > 20000:
-                    epsilon -= 0.00000075
+                    epsilon -= 0.00001 # 0.00000085
                     epsilon = np.maximum(min_epsilon,epsilon)
                     train_indicator = True
                 else:
@@ -93,10 +95,10 @@ def trainer(epochs=1000, MINIBATCH_SIZE=32, GAMMA = 0.99,save=1, save_image=1, e
                 
                 
                 # for visualization
-                #numpy_horizontal = np.hstack((np.array(state)[:,:,0], np.array(state)[:,:,1], np.array(state)[:,:,2],np.array(state)[:,:,3]))
-                #cv2.imshow('image', numpy_horizontal)
-                #cv2.waitKey(1)
-                #time.sleep(0.5)
+                # numpy_horizontal = np.hstack((np.array(state)[:,:,0], np.array(state)[:,:,1], np.array(state)[:,:,2],np.array(state)[:,:,3]))
+                # cv2.imshow('image', numpy_horizontal)
+                # cv2.waitKey(1)
+                # time.sleep(0.05)
                 
                 # 1. get action with e greedy
                 if np.random.random_sample() < epsilon:
@@ -105,9 +107,11 @@ def trainer(epochs=1000, MINIBATCH_SIZE=32, GAMMA = 0.99,save=1, save_image=1, e
                 else:
                     # Just stick to what you know bro
                     
-                    q0 = agent.predict(np.reshape(np.array(state).astype(np.uint8),[-1,SIZE_FRAME,SIZE_FRAME,4])) 
+                    q0, X = agent.predict(np.reshape(np.array(state).astype(np.uint8),[-1,SIZE_FRAME,SIZE_FRAME,4])) 
+                    
                     action = np.argmax(q0)
-                
+                    # print(q0)
+
                 
                 
                 next_state, reward, done, info = env.step(action)#env.step(action)
@@ -125,17 +129,21 @@ def trainer(epochs=1000, MINIBATCH_SIZE=32, GAMMA = 0.99,save=1, save_image=1, e
                         
                         
                         q_eval = agent.predict_target(s2_batch)
-                        q_target = q_eval.copy()
+                        #q_target = q_eval.copy()
+                        q_target = np.zeros(MINIBATCH_SIZE)
+
                         for k in range(MINIBATCH_SIZE):
                             if t_batch[k]:
                                 #print(q_target[k])
-                                q_target[k][a_batch[k]] = r_batch[k]
+                                q_target[k] = r_batch[k]
                             else:
                                 # TODO check that we are grabbing the max of the triplet
-                                q_target[k][a_batch[k]] = r_batch[k] + GAMMA * np.max(q_eval[k])
+                                q_target[k] = r_batch[k] + GAMMA * np.max(q_eval[k])
                         #5.3 Train agent! 
-                        agent.train(np.reshape(a_batch,(MINIBATCH_SIZE,1)),np.reshape(q_target,(MINIBATCH_SIZE,N_ACTIONS)), s_batch )
-                
+                        #print(q_target, a_batch)
+                        loss, _ = agent.train(np.reshape(a_batch,(MINIBATCH_SIZE,1)),np.reshape(q_target,(MINIBATCH_SIZE,1)), s_batch )
+                        # target_final, q_acted, delta, loss, optimize = agent.train_v2(np.reshape(a_batch,(MINIBATCH_SIZE,1)),np.reshape(q_target,(MINIBATCH_SIZE,1)), s_batch )
+                        # print('target_final', target_final, 'q_acted', q_acted, 'delta', delta, 'loss', loss)
                 # 3. Save in replay buffer:
                 replay_buffer.add(state,action,reward,done,next_state) 
                 
@@ -144,9 +152,10 @@ def trainer(epochs=1000, MINIBATCH_SIZE=32, GAMMA = 0.99,save=1, save_image=1, e
                 state = next_state
                 ep_reward = ep_reward + reward
                 step +=1
-                
+                total_loss.append(loss)
             
-            print('th',total_frames_counter+1,'Step', step,'Reward:',ep_reward,'epsilon', round(epsilon,3), algo)
+            print('th',total_frames_counter+1,'Step', step,'Reward:',ep_reward,'epsilon', round(epsilon,3), np.mean(total_loss))
+
             # print('the reward at the end of the episode,', reward)
             
                         
@@ -162,4 +171,4 @@ def trainer(epochs=1000, MINIBATCH_SIZE=32, GAMMA = 0.99,save=1, save_image=1, e
 
 
 if __name__ == '__main__':
-    trainer(epochs=20000 ,save_image = False, epsilon= 1., train_indicator = True) # 0.8
+    trainer(epochs=20000 ,save_image = False, epsilon= 0.001, train_indicator = True) # 0.8

q_network.py
@@ -2,7 +2,7 @@
 import tensorflow as tf
 import numpy as np
 
-
+EPSILON = 0.1
 
 class Network(object):
     """
@@ -21,38 +21,52 @@ class Network(object):
         self.SIZE_FRAME = SIZE_FRAME
         
         # Q network
-        self.inputs, self.out, self.saver = self.create_q_network('q')
+        self.inputs, self.out, self.saver, self.X = self.create_q_network('q')
         self.network_params = tf.trainable_variables()
 
         # Target Network
-        self.target_inputs, self.target_out, self.target_saver = self.create_q_network('q_target')
+        self.target_inputs, self.target_out, self.target_saver, self.target_X = self.create_q_network('q_target')
 
-        self.target_network_params = tf.trainable_variables()[
-            len(self.network_params):]
+        self.target_network_params = tf.trainable_variables()[len(self.network_params):]
 
         # Op for periodically updating target network with online network
         # weights
-        self.update_target_network_params = [self.target_network_params[i].assign(self.network_params[i]) for i in range(len(self.target_network_params))]
+        #self.update_target_network_params = [self.target_network_params[i].assign(self.network_params[i]) for i in range(len(self.target_network_params))]
+        self.update_target_network_params = [oldp.assign(p) for p, oldp in zip(self.network_params, self.target_network_params)] # vf_params, vf_old_params
+
+
+        #self.global_step = tf.train.get_or_create_global_step()
+        #epsilon_decay = tf.train.polynomial_decay(EPSILON, self.global_step, 1e5, 0.01, power=0.0)
+
 
         with tf.device(self.device):
 
-            self.target_q_t = tf.placeholder(tf.float32, [None, self.a_dim], name='target_q')
-            self.action = tf.placeholder(tf.int32, [None, 1])
+            self.target_q_t = tf.placeholder(tf.float32, [None, 1], name='target_q')
+            self.action = tf.placeholder(tf.int32, [None, 1], name='action')
             # obtain the q scores of the selected action
-            action_one_hot = tf.one_hot(self.action, self.a_dim, 1.0, 0.0, name='action_one_hot')
-            q_acted = tf.reduce_sum(self.out * action_one_hot, reduction_indices=1, name='q_acted')
-            
-            self.delta = tf.subtract(tf.stop_gradient(self.target_q_t), q_acted)
+            self.action_one_hot = tf.one_hot(self.action, self.a_dim, name='action_one_hot')
+            self.q_acted_0 = self.out * tf.squeeze(self.action_one_hot)
+            self.q_acted = tf.reduce_sum(self.q_acted_0, reduction_indices=1, name='q_acted')
+
+            #self.action_one_hot = tf.one_hot(self.action, self.a_dim, 1.0, 0.0, name='action_one_hot')
+            #self.q_acted = tf.reduce_sum(self.out * self.action_one_hot, reduction_indices=1, name='q_acted')
+            #self.q_reduced = tf.reduce_mean(self.q_acted, axis=1)
+
+            #self.delta = tf.subtract(tf.stop_gradient(self.target_q_t), q_acted)
             #self.loss = self.clipped_error(self.delta)
+            #self.loss = tf.reduce_mean(self.clipped_error(self.delta), name='loss')
+            self.target_final = tf.squeeze(self.target_q_t)
+            self.delta = tf.subtract(tf.stop_gradient(self.target_final), self.q_acted)
             self.loss = tf.reduce_mean(self.clipped_error(self.delta), name='loss')
-            
+
+            #self.loss = tf.losses.huber_loss(self.target_final, self.q_acted, reduction=tf.losses.Reduction.MEAN)
             self.optimizer = tf.train.AdamOptimizer(self.learning_rate)
             gradients = self.optimizer.compute_gradients(self.loss)
-            #for i, (grad, var) in enumerate(gradients):
-            #    if grad is not None:
-            #        gradients[i] = (tf.clip_by_norm(grad, 5.), var)
+            for i, (grad, var) in enumerate(gradients):
+                if grad is not None:
+                    gradients[i] = (tf.clip_by_norm(grad, 5.), var)
             self.optimize = self.optimizer.apply_gradients(gradients)
-
+            
 
 
         self.num_trainable_vars = len(self.network_params) + len(self.target_network_params)
@@ -96,15 +110,15 @@ class Network(object):
                 '''
                 X = tf.to_float(stateInput) / 255.0
                 #X = tf.reshape(stateInput,shape=[-1, self.SIZE_FRAME, self.SIZE_FRAME, 4])
-                conv1 = tf.contrib.layers.conv2d(X, 32, 8, 4, activation_fn=tf.nn.relu)
-                conv2 = tf.contrib.layers.conv2d(conv1, 64, 4, 2, activation_fn=tf.nn.relu)
-                conv3 = tf.contrib.layers.conv2d(conv2, 64, 3, 1, activation_fn=tf.nn.relu)
+                conv1 = tf.contrib.layers.conv2d(X, 32, 8, 4, activation_fn=tf.nn.leaky_relu)
+                conv2 = tf.contrib.layers.conv2d(conv1, 64, 4, 2, activation_fn=tf.nn.leaky_relu)
+                conv3 = tf.contrib.layers.conv2d(conv2, 64, 3, 1, activation_fn=tf.nn.leaky_relu)
 
                 # fully connected layer
 
                 flattened = tf.contrib.layers.flatten(conv3)
-                fc = tf.contrib.layers.fully_connected(flattened, 512)
-                out = tf.contrib.layers.fully_connected(fc, self.a_dim,activation_fn=None)
+                fc = tf.contrib.layers.fully_connected(flattened, 512, activation_fn=tf.nn.leaky_relu)
+                out = tf.contrib.layers.fully_connected(fc, self.a_dim, activation_fn=None)
                 #out = tf.contrib.layers.fully_connected(fc, self.a_dim, activation_fn=None)
                 # out = tf.matmul(fc, W_out)+ b_out
                 #fc = tf.nn.relu(tf.matmul(flattened,W_fc)+ b_fc)
@@ -137,12 +151,20 @@ class Network(object):
                 out = tf.matmul(h_fc1,W_fc2) + b_fc2
                 '''
         saver = tf.train.Saver()
-        return stateInput, out, saver
+        return stateInput, out, saver, conv3
 
         
     def train(self, actions, target_q_t, inputs):
         with tf.device(self.device):
-            self.sess.run(self.optimize, feed_dict={
+            return self.sess.run([self.loss, self.optimize], feed_dict={
+                self.action: actions,
+                self.target_q_t: target_q_t,
+                self.inputs: inputs
+            })
+
+    def train_v2(self, actions, target_q_t, inputs):
+        with tf.device(self.device):
+            return self.sess.run([self.target_final, self.q_acted, self.delta, self.loss, self.optimize], feed_dict={
                 self.action: actions,
                 self.target_q_t: target_q_t,
                 self.inputs: inputs
@@ -150,7 +172,7 @@ class Network(object):
 
     def predict(self, inputs):
         with tf.device(self.device):
-            return self.sess.run(self.out, feed_dict={
+            return self.sess.run([self.out, self.X], feed_dict={
                 self.inputs: inputs
             })
 

wrappers.py
@@ -193,8 +193,9 @@ class ProcessFrame84(gym.ObservationWrapper):
         x_t = resized_screen[18:102, :]
         x_t = np.reshape(x_t, [84, 84, 1])
         x_t = x_t.astype(np.uint8)
-        #cv2.imshow('image', x_t)
-        #cv2.waitKey(1)
+        # cv2.imshow('image', frame)
+        # cv2.imshow('image', x_t)
+        # cv2.waitKey(1)
         return x_t# .astype(np.uint8)
 
 